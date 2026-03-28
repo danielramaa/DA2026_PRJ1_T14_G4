c9ace68af138a34da759e3c90d9e743934a450eb
danielramaa <danie.morais.rama@gmail.com>
2026-03-28 23:57:46 +0000

    add parser header and update CMakeLists.txt

---
+++ include/parser.h
@@ -0,0 +1,13 @@
+//
+// Created by danielramaa on 3/28/26.
+//
+
+#ifndef DA2026_PRJ1_T14_G3_PARSER_H
+#define DA2026_PRJ1_T14_G3_PARSER_H
+
+#include "types.h"
+#include <string>
+
+Dataset parseInputFile(const std::string& filename);
+
+#endif
